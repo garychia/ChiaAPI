4e8445a3f26c83e5f0427c1341eacb347988a7b9
Chia-Hsiang Cheng <s940046@hotmail.com>
2022-11-18 21:50:56 +0800

    Update documentation

--- ChiaData/include/Array.hpp
+++ ChiaData/include/Array.hpp
@@ -5,12 +5,26 @@
 
 namespace ChiaData
 {
+/**
+ * @brief Represents an array of a fix length.
+ *
+ * @tparam T the type of elements in the array.
+ */
 template <class T> class Array
 {
   protected:
+    /**
+     * @brief where the elements are stored.
+     */
     T *data = 0;
+    /**
+     * @brief the number of elements stored.
+     */
     size_t length = 0;
 
+    /**
+     * @brief Release the elements stored
+     */
     inline void ReleaseData()
     {
         if (data)
@@ -19,18 +33,35 @@ template <class T> class Array
         length = 0;
     }
 
+    /**
+     * @brief Allocate memory for the array.
+     *
+     * @param size the number of elements to store.
+     */
     inline void AllocateData(size_t size)
     {
         length = size;
         data = length ? new T[length]{} : 0;
     }
 
+    /**
+     * @brief Copy elements from another array
+     *
+     * @param newData the pointer to the other array.
+     * @param size the number of elements to copy.
+     */
     inline void CopyData(const T *newData, size_t size)
     {
         for (size_t i = 0; i < size; i++)
             data[i] = newData[i];
     }
 
+    /**
+     * @brief Populate the array with a value
+     *
+     * @param element the value used to populate the array.
+     * @param size the number of elements to store in the array.
+     */
     inline void CopyData(const T &element, size_t size)
     {
         for (size_t i = 0; i < size; i++)
@@ -38,28 +69,52 @@ template <class T> class Array
     }
 
   public:
+    /**
+     * @brief type of the elements in the array.
+     */
     using ValueType = T;
 
+    /**
+     * @brief Construct a new Array object
+     */
     Array() noexcept
     {
     }
 
+    /**
+     * @brief Destroy the Array object
+     */
     virtual ~Array() noexcept
     {
         ReleaseData();
     }
 
+    /**
+     * @brief Construct a new Array object
+     *
+     * @param initialSize the number of elements to store in the array.
+     */
     Array(size_t initialSize) noexcept
     {
         AllocateData(initialSize);
     }
 
+    /**
+     * @brief Construct a new Array object
+     *
+     * @param arr the array to be copied into this array.
+     */
     Array(const Array &arr) noexcept
     {
         AllocateData(arr.Length());
         CopyData(arr.data, arr.Length());
     }
 
+    /**
+     * @brief Construct a new Array object
+     *
+     * @param initList an initializer_list whose elements will be copied into this array.
+     */
     Array(const std::initializer_list<T> &initList)
     {
         AllocateData(initList.size());
@@ -68,6 +123,11 @@ template <class T> class Array
             data[idx++] = element;
     }
 
+    /**
+     * @brief Construct a new Array object
+     *
+     * @param arr the array to be 'moved' into this array.
+     */
     Array(Array &&arr) noexcept
     {
         length = arr.Length();
@@ -76,18 +136,36 @@ template <class T> class Array
         arr.data = 0;
     }
 
+    /**
+     * @brief Construct a new Array object
+     *
+     * @param element the value used to populate the array.
+     * @param nElements the number of elements the array will have.
+     */
     Array(const T &element, size_t nElements) noexcept
     {
         AllocateData(nElements);
         CopyData(element, nElements);
     }
 
+    /**
+     * @brief Construct a new Array object
+     *
+     * @param cArr the pointer to the array to be copied into this array.
+     * @param nElements the number of elements this array will have.
+     */
     Array(const T *cArr, size_t nElements) noexcept
     {
         AllocateData(nElements);
         CopyData(cArr, nElements);
     }
 
+    /**
+     * @brief Array Copy Assignment
+     *
+     * @param arr the array to be copied into this array.
+     * @return Array<T>& this array.
+     */
     Array<T> &operator=(const Array<T> &arr) noexcept
     {
         ReleaseData();
@@ -96,6 +174,12 @@ template <class T> class Array
         return *this;
     }
 
+    /**
+     * @brief Array Move Assignment
+     *
+     * @param arr the array to be 'moved' into this array.
+     * @return Array<T>& this array.
+     */
     Array<T> &operator=(Array<T> &&arr) noexcept
     {
         ReleaseData();
@@ -106,6 +190,14 @@ template <class T> class Array
         return *this;
     }
 
+    /**
+     * @brief Check if two arrays have the same number of equivalent elements
+     *
+     * @tparam Comparator the type of comparator that takes two elements and returns true if they are equivalent.
+     * @param other another array.
+     * @return true if both of the arrays have the same number of equivalent elements.
+     * @return false otherwise.
+     */
     template <class Comparator> bool operator==(const Array<T> &other) const noexcept
     {
         Comparator cmp;
@@ -119,46 +211,93 @@ template <class T> class Array
         return true;
     }
 
+    /**
+     * @brief Get the pointer of the first element
+     *
+     * @return T* the pointer of the first element.
+     */
     inline T *operator*() noexcept
     {
         return data;
     }
 
+    /**
+     * @brief Get the pointer of the first element
+     *
+     * @return const T* the pointer of the first element.
+     */
     inline const T *operator*() const noexcept
     {
         return data;
     }
 
+    /**
+     * @brief Get the element at the given index
+     *
+     * @param index the index.
+     * @return T& the element at the index.
+     */
     inline T &operator[](size_t index) noexcept
     {
         return data[index];
     }
 
+    /**
+     * @brief Get the element at the given index
+     *
+     * @param index the index.
+     * @return const T& the element at the index.
+     */
     inline const T &operator[](size_t index) const noexcept
     {
         return data[index];
     }
 
+    /**
+     * @brief Get the number of elements in the array
+     *
+     * @return size_t the number of elements in the array.
+     */
     inline virtual size_t Length() const noexcept
     {
         return length;
     }
 
+    /**
+     * @brief Get the first element in the array
+     *
+     * @return T& the first element in the array.
+     */
     inline virtual T &GetFirst() noexcept
     {
         return data[0];
     }
 
+    /**
+     * @brief Get the first element in the array
+     *
+     * @return const T& the first element in the array.
+     */
     inline virtual const T &GetFirst() const noexcept
     {
         return data[0];
     }
 
+    /**
+     * @brief Get the last element in the array
+     *
+     * @return T& the last element in the array.
+     */
     inline virtual T &GetLast() noexcept
     {
         return data[length - 1];
     }
 
+    /**
+     * @brief Get the last element in the array
+     *
+     * @return const T& the last element in the array
+     */
     inline virtual const T &GetLast() const noexcept
     {
         return data[length - 1];
