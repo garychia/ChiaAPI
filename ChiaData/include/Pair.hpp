7d5f827b2022d6e44dd77d6a7e5ba59edf4d05b8
Chia-Hsiang Cheng <s940046@hotmail.com>
2023-01-13 21:36:56 +0800

    Update documentation

--- ChiaData/include/Pair.hpp
+++ ChiaData/include/Pair.hpp
@@ -6,31 +6,59 @@ namespace ChiaData
 template <class T, class U> class Pair
 {
   private:
+    // the first value of pair.
     T key;
 
+    // the second value of pair.
     U value;
 
   public:
+    /**
+     * @brief Retrieve the key of the pair.
+     *
+     * @return T& the key.
+     */
     T &Key()
     {
         return key;
     }
 
+    /**
+     * @brief Retrieve the key of the pair.
+     *
+     * @return const T& the key.
+     */
     const T &Key() const
     {
         return key;
     }
 
+    /**
+     * @brief Retrieve the value of the pair.
+     *
+     * @return U& the value.
+     */
     U &Value()
     {
         return value;
     }
 
+    /**
+     * @brief Retrieve the value of the pair.
+     *
+     * @return const U& the value.
+     */
     const U &Value() const
     {
         return value;
     }
 
+    /**
+     * @brief Copy Assignment of Pair
+     *
+     * @param other the pair to be copied.
+     * @return Pair& this pair.
+     */
     Pair &operator=(const Pair &other)
     {
         key = other.key;
