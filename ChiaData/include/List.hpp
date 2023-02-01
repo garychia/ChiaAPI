1da2488911013417807be83b66fa11fbd2f9201f
Chia-Hsiang Cheng <gary.chiahsiang.cheng@gmail.com>
2023-02-01 23:23:55 +0800

    Update documentation

--- ChiaData/include/List.hpp
+++ ChiaData/include/List.hpp
@@ -7,25 +7,64 @@
 
 namespace ChiaData
 {
+/**
+ * @brief A data structure where data can be inserted at the beginning or the end.
+ *
+ * @tparam T the type of the elements.
+ */
 template <class T> class List
 {
   public:
     class Iterator;
 
   private:
+    /**
+     * @brief Represents an entry of the List.
+     */
     struct Element
     {
+        /**
+         * @brief the data this Element stores.
+         */
         T data;
+
+        /**
+         * @brief the List that stores this Element.
+         */
         List<T> *owner;
+
+        /**
+         * @brief the next Element.
+         */
         Element *next;
+
+        /**
+         * @brief the previous Element.
+         */
         Element *prev;
 
+        /**
+         * @brief Construct a new Element object
+         *
+         * @tparam DataType the type of the data the Element stores.
+         * @param data the data to be stored.
+         * @param owner the List this Element belongs to.
+         * @param prev the previous Element.
+         * @param next the next Element.
+         */
         template <class DataType>
         Element(DataType &&data, List<T> *owner, Element *prev = nullptr, Element *next = nullptr)
             : data(Types::Forward<T>(data)), owner(owner), next(next), prev(prev)
         {
         }
 
+        /**
+         * @brief Move an Element object.
+         *
+         * @tparam ElementType the type of data to be stored.
+         * @param e the Element to be moved.
+         * @return Element& this Element.
+         */
         template <class ElementType> Element &operator=(ElementType &&e)
         {
             data = Types::Forward<decltype(e)>(e.data);
@@ -34,21 +73,41 @@ template <class T> class List
             owner = e.owner;
         }
 
+        /**
+         * @brief Retrieve the data.
+         *
+         * @return T& the data.
+         */
         T &operator*()
         {
             return data;
         }
 
+        /**
+         * @brief Retrieve the data.
+         * 
+         * @return const T& the data.
+         */
         const T &operator*() const
         {
             return data;
         }
 
+        /**
+         * @brief Access the data.
+         * 
+         * @return T* the data.
+         */
         T *operator->()
         {
             return &data;
         }
 
+        /**
+         * @brief Access the data.
+         * 
+         * @return const T* the data.
+         */
         const T *operator->() const
         {
             return &data;
